61f4d63cb2021934c4097e9b7ccd491f9ff71e50
brendojn <brendojnio@gmail.com>
2019-05-15 21:48:04 -0300

    Inclusão do token_secret

--- cabeleireiro/authenticate.js
+++ cabeleireiro/authenticate.js
@@ -1,6 +1,7 @@
 const auth = require('./cabeleireiro.modelo');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 module.exports = {
 
@@ -10,7 +11,7 @@ module.exports = {
                 next(err);
             } else {
                 if(bcrypt.compareSync(req.body.senha, info.senha)) {
-                    const token = jwt.sign({id: info._id}, req.app.get('secretKey'), { expiresIn: '2h' });
+                    const token = jwt.sign({id: info._id}, config.TOKEN_SECRET, { expiresIn: '2h' });
                     res.json({status:"success", message: "Usuário encontrado!", data:{user: info, token:token}});
                 }else{
                     res.json({status:"error", message: "Email/Senha inválidos!", data:null});
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+// 2. JWT
+    TOKEN_SECRET: process.env.TOKEN_SECRET || 'LVKIn2D8CazF1HXPdtMhWkCjbSMGpcMJodeuulRkofyzB7O5DiSsRxWJNL2I5OQ'
+
+
+};
