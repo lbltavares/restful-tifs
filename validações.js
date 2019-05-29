f2fd78819623bb039399e5b241ca9f406bc74f94
lbltavares <lucas.laborne@gmail.com>
2019-05-29 16:42:48 -0300

    Adicionado metodo de validação de Email

--- validações.js
+++ validações.js
@@ -30,7 +30,8 @@ function validarCpf(cpf) {
 }
 
 function validarEmail(cpf) {
-
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 module.exports = {
